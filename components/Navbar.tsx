5b2386d01e99854da64ad9f5dd01dec64625e7c6
Arjun <arjunsingh8112@gmail.com>
2023-07-04 12:49:47 +0530

    Render constants on xl screens

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { NavLinks } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className='flexBetween navbar' >
+            <div className='flex-1 flexStart gap-10'>
+                <Link href="/">
+                    <Image src="/logo.svg" alt="Flexibble" width={115} height={43} />
+                </Link>
+                <ul className="xl:flex hidden text-small gap-7">
+                    {NavLinks.map((link) => (
+                        <Link href={link.href} key={link.key}>
+                            {link.text}
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+        </nav>
     )
 }
 
