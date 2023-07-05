3f042f777f4be70e0f43091eb7b79ceb93f6a1c3
Arjun <arjunsingh8112@gmail.com>
2023-07-05 11:36:15 +0530

    Fix typo

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -3,13 +3,14 @@
 import { getProviders, signIn } from "next-auth/react"
 import { useState, useEffect } from "react"
 
-type Providers = {
+type Provider = {
     id: string
     name: string
     signInUrl: string
     callbackUrl: string
     signinUrlParams: Record<string, string> | null
 }
+
 
 const AuthProviders = () => {
     const [Providers, setProviders] = useState<Providers | null>(null)
