eb2e8269b96c32f0d411a1940b4a8e7520fab1e1
Arjun <arjunsingh8112@gmail.com>
2023-07-05 10:54:46 +0530

    Create Project model

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -10,6 +10,16 @@ const User = g.model('User', {
   projects: g.relation(() => Project).list().optional(),
 })
 
+const Project = g.model('Project', {
+  title: g.string().length({ min: 3, max: 50 }),
+  description: g.string(),
+  image: g.url().optional(),
+  liveSite: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.relation(()=> User),
+})
+
 export default config({
   schema: g
 })
