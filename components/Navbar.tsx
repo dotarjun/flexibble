4a87945f8a1368d3284dabe6271790dd909cf15f
Arjun <arjunsingh8112@gmail.com>
2023-07-05 10:41:56 +0530

    Desktop Navbar done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,11 @@
 import { NavLinks } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
+import AuthProviders from './AuthProviders'
 
 const Navbar = () => {
+    const session = {}
+
     return (
         <nav className='flexBetween navbar' >
             <div className='flex-1 flexStart gap-10'>
@@ -16,6 +19,19 @@ const Navbar = () => {
                         </Link>
                     ))}
                 </ul>
+            </div>
+            <div className='flexCenter gap-4 '>
+                {session ? (
+                    <>
+                        UserPhoto
+
+                        <Link href="/create-project">
+                            Share Work
+                        </Link>
+                    </>
+                ) : (
+                    <AuthProviders />
+                )}
             </div>
         </nav>
     )
