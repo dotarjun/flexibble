65a7e4096c19efe3568a359039dd11d7b07ab222
Arjun <arjunsingh8112@gmail.com>
2023-07-05 11:07:24 +0530

    create Providers type and state

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,6 +1,20 @@
-import React from 'react'
+"use client"
+
+import { getProviders, signIn } from "next-auth/react"
+import { useState, useEffect } from "react"
+
+type Providers = {
+    id: string
+    name: string
+    signInUrl: string
+    callbackUrl: string
+    signinUrlParams: Record<string, string> | null
+}
 
 const AuthProviders = () => {
+    const [Providers, setProviders] = useState<Providers | null>(null)
+
+
     return (
         <div>AuthProviders</div>
     )
