bfdab0a38cd156631c05b76b0defe9ff0d8e1377
Arjun <arjunsingh8112@gmail.com>
2023-07-05 10:54:35 +0530

    create User model

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,5 +1,15 @@
 import { g, auth, config } from '@grafbase/sdk'
 
+const User = g.model('User', {
+  name: g.string().length({ min: 2, max: 20 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects: g.relation(() => Project).list().optional(),
+})
+
 export default config({
   schema: g
 })
