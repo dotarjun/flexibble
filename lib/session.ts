acec9c6f7d75f11c4783c197359abe1fa6fa2c0c
Arjun <arjunsingh8112@gmail.com>
2023-07-19 10:01:04 +0530

    feat: signInUser and update session and merge with DB

--- lib/session.ts
+++ lib/session.ts
@@ -2,9 +2,11 @@ import { getServerSession } from "next-auth/next";
 import { NextAuthOptions, User } from "next-auth";
 import { AdapterUser } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
-import jsonwebtoken from "jsonwebtoken";
-import { JWT } from "next-auth/jwt";
-import { SessionInterface } from "@/common.types";
+// import jsonwebtoken from "jsonwebtoken";
+// import { JWT } from "next-auth/jwt";
+
+import { SessionInterface, UserProfile } from "@/common.types";
+import { createUser, getUser } from "./actions";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -25,10 +27,38 @@ export const authOptions: NextAuthOptions = {
   },
   callbacks: {
     async session({ session }) {
+      const email = session?.user?.email as string;
+
+      try {
+        const data = (await getUser(email)) as { user?: UserProfile };
+
+        // Update the session object with the user data from the database
+        const newSession = {
+          ...session,
+          user: {
+            ...session.user,
+            ...data.user,
+          },
+        };
+        return newSession;
+      } catch (error) {
+        console.log("Error retreiving user Data", error);
+      }
       return session;
     },
+
     async signIn({ user }: { user: AdapterUser | User }) {
       try {
+        const userExists = (await getUser(user?.email as string)) as {
+          user?: UserProfile;
+        };
+        if (!userExists) {
+          await createUser(
+            user.name as string,
+            user.email as string,
+            user.image as string
+          );
+        }
         return true;
       } catch (error) {
         console.log(error);
